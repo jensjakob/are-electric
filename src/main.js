498bc14eec7f1322a9a5896a6fb60c7b932a9df5
jensjakob <jensjakob@users.noreply.github.com>
2018-06-17 14:44:32 +0200

    adding firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import Vue from 'vue';
+import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+const config = {
+  apiKey: 'AIzaSyCBvuRlPH1OMhsGf3d7_C9UbeGMMpFodro',
+  authDomain: 'are-electric.firebaseapp.com',
+  databaseURL: 'https://are-electric.firebaseio.com',
+  projectId: 'are-electric',
+  storageBucket: '',
+  messagingSenderId: '302291386863',
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
