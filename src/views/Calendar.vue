30ea2dd2b0de100ccc2663144637ebd271891769
jensjakob <jensjakob@users.noreply.github.com>
2018-07-22 14:44:09 +0200

    Keep view updated with selections

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -3,8 +3,8 @@
 
     <div>
       <p v-for="(item, index) in items" v-bind:key="item.id">
-        <input type="radio" name="item" v-model.number="selectedItem" v-bind:value="index">
-          {{ item.title }}<br>
+        <label><input type="radio" name="item" v-model.number="selectedItem" v-bind:value="index">
+          {{ item.title }}</label><br>
           {{ item.price }} SEK
       </p>
     </div>
@@ -36,10 +36,15 @@
 
     <div>
       <p>Pickup: <input type="date" v-model="fromDay"> between 9-11 am</p>
-      <p v-if="fromDay">Leave: <input type="date" v-model="toDay"> between 3-4 pm</p>
+      <p v-if="fromDay || toDay">Leave: <input type="date" v-model="toDay"> between 3-4 pm</p>
     </div>
 
-    <div v-if="selectedItem !== undefined">
+    <div v-if="selectedItem === undefined && calcDays > 0">
+      <h2>Price</h2>
+      <p>Please select a car to calculate the total price.</p>
+    </div>
+
+    <div v-if="selectedItem !== undefined && calcDays > 0">
       <h2>Price</h2>
       <p>{{ calcDays }} days à {{ items[selectedItem].price }} SEK = {{ calcPrice }} SEK</p>
     </div>
@@ -47,7 +52,7 @@
     <div>
       <p>E-mail: <input type="email" v-model="email"></p>
       <p>Phone: <input type="tel" v-model="phone"></p>
-      <p><input type="checkbox" v-model="agree"> Agree terms</p>
+      <p><label><input type="checkbox" v-model="agree"> Agree terms</label></p>
       <button v-bind:disabled="selectedItem === undefined || !fromDay || !toDay || !email || !phone || !agree" v-on:click="saveReservation">Make reservation</button>
     </div>
 
@@ -55,13 +60,14 @@
 </template>
 
 <script>
+const today = new Date();
+today.setHours(0, 0, 0, 0);
+
 export default {
   data() {
     return {
       fromDay: '',
       toDay: '',
-      calcDays: 0,
-      calcPrice: 0,
       selectedItem: undefined,
       items: [
         {
@@ -87,26 +93,71 @@ export default {
       updated: undefined,
     };
   },
+  computed: {
+    // eslint-disable-next-line
+    calcDays: function () {
+      if (this.fromDay && this.toDay) {
+        return this.$moment(this.toDay).diff(this.fromDay, 'days');
+      }
+      return 0;
+    },
+    // eslint-disable-next-line
+    calcPrice: function () {
+      if (this.selectedItem !== undefined) {
+        return this.calcDays * this.items[this.selectedItem].price;
+      }
+      return 0;
+    },
+  },
   watch: {
     // eslint-disable-next-line
-    fromDay: function (val) {
+    selectedItem: function (val) {
+
+      // clear all selected days
       Object.keys(this.calendar).forEach((key) => {
-        this.calendar[key].selected = false;
+        this.calendar[key].booked = false;
       });
+
+      // blocka bokade dagar
+      this.$db.collection('calendar').where('day', '>=', today).where('item', '==', this.items[this.selectedItem].name).orderBy('day', 'asc')
+        .onSnapshot((snapshot) => {
+          console.log('snap', this.items[this.selectedItem].name);
+          snapshot.docChanges().forEach((change) => {
+            const theDay = this.$moment.unix(change.doc.data().day.seconds).format('YYYY-MM-DD');
+            // console.log(theDay, change.type, change.doc.data().status);
+            if (change.type === 'added') {
+              if (change.doc.data().status === 'booked') {
+                this.calendar[theDay].booked = true; // will not update view
+                this.$forceUpdate(); // this will update view
+              }
+            }
+            if (change.type === 'removed') {
+              //
+            }
+          });
+        });
+    },
+    // eslint-disable-next-line
+    fromDay: function (val) {
+      // Object.keys(this.calendar).forEach((key) => {
+      //   this.calendar[key].selected = false;
+      // });
       this.calendar[val].selected = true;
-      if (this.toDay) {
-        this.calcDays = this.$moment(this.toDay).diff(this.fromDay, 'days');
-      }
     },
     // eslint-disable-next-line
     toDay: function (val) {
-      if (this.fromDay) {
-        this.calcDays = this.$moment(this.toDay).diff(this.fromDay, 'days');
+      this.calendar[val].selected = true;
+    },
+    // eslint-disable-next-line
+    calcDays: function (val) {
 
-        Object.keys(this.calendar).forEach((key) => {
-          this.calendar[key].selected = false;
-        });
+      // clear all selected days
+      Object.keys(this.calendar).forEach((key) => {
+        this.calendar[key].selected = false;
+      });
 
+      if (this.calcDays > 0) {
+        // make selection
         const date = new Date(this.fromDay);
         for (let i = 0; i <= this.calcDays; i += 1) {
           if (this.calendar[this.$moment(date).format('L')].booked) {
@@ -117,13 +168,9 @@ export default {
           date.setDate(date.getDate() + 1);
         }
 
-        if (this.selectedItem !== undefined) {
-          this.calcPrice = this.calcDays * this.items[this.selectedItem].price;
-        }
-
-        if (this.fromDay >= this.toDay) {
-          this.toDay = this.$moment(this.fromDay).add(1, 'days').format('L');
-        }
+        // if (this.fromDay >= this.toDay) {
+        //   this.toDay = this.$moment(this.fromDay).add(1, 'days').format('L');
+        // }
 
         // if (this.calcDays >= 0) {
         //   this.calcDays = 0;
@@ -131,14 +178,14 @@ export default {
       }
     },
   },
+  updated() {
+
+  },
   mounted() {
     // this.$db.collection('reservations').doc('72fU4UIue3vsaXXGebTZ')
     // .onSnapshot((snap) => {
     //   // this.email = snap.data().email;
     // });
-
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
 
     const firstDay = this.$moment(today).weekday('-0');
     const lastDay = this.$moment().add(1, 'M').endOf('month');
@@ -165,22 +212,16 @@ export default {
       date.setDate(date.getDate() + 1);
     }
 
-    // where('item', '==', 'tesla').
-    // if (this.selectedItem !== undefined) {
-    //   let selection = this.items[this.selectedItem].name;
-    // } else {
-    //   let selection
-    // }
-
+    // blocka bokade dagar
     this.$db.collection('calendar').where('day', '>=', today).orderBy('day', 'asc')
-      .onSnapshot((querySnapshot) => {
-        querySnapshot.docChanges().forEach((change) => {
+      .onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
           const theDay = this.$moment.unix(change.doc.data().day.seconds).format('YYYY-MM-DD');
           // console.log(theDay, change.type, change.doc.data().status);
           if (change.type === 'added') {
             if (change.doc.data().status === 'booked') {
-              this.calendar[theDay].booked = true;
-              this.$forceUpdate();
+              this.calendar[theDay].booked = true; // will not update view
+              this.$forceUpdate(); // this will update view
             }
           }
           if (change.type === 'removed') {
